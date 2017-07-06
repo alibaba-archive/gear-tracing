fb809c023c6ea7f0f1bd478eaac72581b2ba43f7
evolsnow <gsc1215225@gmail.com>
2017-07-06 14:00:35 +0800

    Extracting SpanContext from Request

--- tracing.go
+++ tracing.go
@@ -10,7 +10,20 @@ import (
 // New returns a tracing middleware
 func New(opts ...opentracing.StartSpanOption) gear.Middleware {
 	return func(ctx *gear.Context) error {
-		span := opentracing.StartSpan(fmt.Sprintf(`%s %s`, ctx.Method, ctx.Path), opts...)
+		var span opentracing.Span
+		opName := fmt.Sprintf(`%s %s`, ctx.Method, ctx.Path)
+		// Attempt to join a trace by getting trace context from the headers.
+		wireContext, err := opentracing.GlobalTracer().Extract(
+			opentracing.TextMap,
+			opentracing.HTTPHeadersCarrier(ctx.Req.Header))
+		if err != nil {
+			// If for whatever reason we can't join, go ahead an start a new root span.
+			span = opentracing.StartSpan(opName, opts...)
+		} else {
+			opts = append(opts, opentracing.ChildOf(wireContext))
+			span = opentracing.StartSpan(opName, opts...)
+		}
+
 		ctx.WithContext(opentracing.ContextWithSpan(ctx.Context(), span))
 		ctx.OnEnd(span.Finish)
 		return nil
