3ab3dd536d7106e95eb9824dc7075196a8c8950c
xushuai <dyxushuai@gmail.com>
2017-09-29 11:39:37 +0800

    Simple implemention for more readable

--- tracing.go
+++ tracing.go
@@ -10,24 +10,19 @@ import (
 // New returns a tracing middleware
 func New(opts ...opentracing.StartSpanOption) gear.Middleware {
 	return func(ctx *gear.Context) error {
-		// copy opts avoiding append in the same opts each time.
-		opts := append([]opentracing.StartSpanOption(nil), opts...)
-		var span opentracing.Span
-		opName := fmt.Sprintf(`%s %s`, ctx.Method, ctx.Path)
 		// Attempt to join a trace by getting trace context from the headers.
 		wireContext, err := opentracing.GlobalTracer().Extract(
 			opentracing.HTTPHeaders,
 			opentracing.HTTPHeadersCarrier(ctx.Req.Header))
-		if err != nil {
-			// If for whatever reason we can't join, go ahead an start a new root span.
-			span = opentracing.StartSpan(opName, opts...)
-		} else {
-			opts = append(opts, opentracing.ChildOf(wireContext))
-			span = opentracing.StartSpan(opName, opentracing.ChildOf(wireContext))
+		if err != nil && err != opentracing.ErrSpanContextNotFound {
+			fmt.Println("failed parsing trace information from header ", err)
 		}
-
+		// copy opts avoiding append in the same opts each time.
+		// ChildOf will ignore the nil wireContext.
+		opts := append([]opentracing.StartSpanOption{opentracing.ChildOf(wireContext)}, opts...)
+		span := opentracing.StartSpan(fmt.Sprintf(`%s %s`, ctx.Method, ctx.Path), opts...)
 		ctx.WithContext(opentracing.ContextWithSpan(ctx.Context(), span))
-		ctx.After(span.Finish)
+		ctx.OnEnd(span.Finish)
 		return nil
 	}
 }
